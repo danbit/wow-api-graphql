f295f9f892013df7c32cd061a4877d7af284ac9c
Danilo <daniloesi@gmail.com>
2018-01-31 12:30:40 -0200

    Add ES6 support with Babel Add nodemon and eslint Created a simple express server

---
+++ dist/README.md
@@ -0,0 +1,2 @@
+# wow-api-graphql
+A GraphQL layer for Blizzard's World of Warcraft API.
---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello WoWrld!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000...');
+});
